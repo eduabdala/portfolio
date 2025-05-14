4ee54e4ee2718e2d2ccf9e48792f3e5b3e67df2c
eduabdala <eduardoabdala@icloud.com>
2025-05-14 20:36:11 -0300

    change: change in interface

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -13,7 +13,7 @@ const geistMono = Geist_Mono({
 });
 
 export const metadata: Metadata = {
-  title: "Portfolio Eduardo Abdala",
+  title: "Portfolio E.Abdala",
   description: "Engenheiro de Software",
 };
 
